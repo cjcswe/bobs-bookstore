6bbbba47dee6a32b0cd87ae818b2b9a0492823eb
awstransform <ecs-user@ip-10-0-3-187.ec2.internal>
2026-02-17 18:43:09 +0000

    AWS Transform commit for jobId f1d5f1dd-3ee0-4ede-8b3e-1c810cce83f0 to commit deployment artifacts

--- app/Bookstore.Web/aws-transform-deploy/deploy.ps1
+++ app/Bookstore.Web/aws-transform-deploy/deploy.ps1
@@ -19,7 +19,7 @@ param(
     [string]$S3Folder,
 
     # ECS-specific parameters
-    [string]$StackName = "AWSTransform-Deploy-App-Stack-Bookstore-Web-626f62-c19aa8",
+    [string]$StackName = "AWSTransform-Deploy-App-Stack-Bookstore-Web-636a63-04fdfb",
     [string]$ECSClusterName,
     [string[]]$SubnetIds,
     [string[]]$SecurityGroupIds,
